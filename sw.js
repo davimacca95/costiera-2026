a2d6a8a91ecb284b443452883eff05bce9f71251
davimacca95 <dmacca@amazon.com>
2026-06-21 19:10:49 +0200

    feat: Costiera Amalfitana trip itinerary - 5 days Jul 16-20, 2026

---
+++ sw.js
@@ -0,0 +1,4 @@
+var CACHE='costiera-v1';
+var URLS=['./', './index.html'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
