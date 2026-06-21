e3ee1cc00fb947c95ebb92acbfe0be4fe0441e5a
davimacca95 <dmacca@amazon.com>
2026-06-21 20:16:18 +0200

    fix: bust service worker cache - network-first strategy
    Old SW was cache-first, serving stale index.html.
    Now: network-first (try fetch, fall back to cache for offline).
    Bumped cache version to v8 to invalidate old caches.
    Added skipWaiting() + activate cleanup of old caches.

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-var CACHE='costiera-v1';
+var CACHE='costiera-v8';
 var URLS=['./', './index.html'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
+self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}))});
+self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).then(function(r){var c=r.clone();caches.open(CACHE).then(function(cache){cache.put(e.request,c)});return r}).catch(function(){return caches.match(e.request)}))});
